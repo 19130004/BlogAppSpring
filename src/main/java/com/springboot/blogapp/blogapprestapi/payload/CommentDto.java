ee5ee33100542d568d5b207c06a8c7ebc88187d8
19130004 <19130004@st.hcmuaf.edu.vn>
2023-12-25 11:45:31 +0700

    update validate  for CommentDto and PostDto

--- src/main/java/com/springboot/blogapp/blogapprestapi/payload/CommentDto.java
+++ src/main/java/com/springboot/blogapp/blogapprestapi/payload/CommentDto.java
@@ -1,11 +1,20 @@
 package com.springboot.blogapp.blogapprestapi.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+    @NotEmpty(message = "Name cannot be empty")
     private String name;
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotEmpty(message = "Body cannot be empty")
+    @Size(min = 10, message = "Body must be at least 10 characters")
     private String body;
 }
--- src/main/java/com/springboot/blogapp/blogapprestapi/payload/PostDto.java
+++ src/main/java/com/springboot/blogapp/blogapprestapi/payload/PostDto.java
@@ -1,14 +1,23 @@
 package com.springboot.blogapp.blogapprestapi.payload;
 
 import com.springboot.blogapp.blogapprestapi.entity.Comment;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 import java.util.Set;
 
 @Data
 public class PostDto {
+    private Long id;
+    @NotEmpty(message = "Title cannot be empty")
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
     private String title;
+    @NotEmpty(message = "Description cannot be empty")
+    @Size(min = 10, max = 100, message = "Description must be between 10 and 100 characters")
     private String description;
+    @NotEmpty(message = "Content cannot be empty")
+    @Size(min = 10, message = "Content must be at least 10 characters")
     private String content;
     private Set<CommentDto> comments;
 
