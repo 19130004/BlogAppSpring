73eb26ad7e058eaeae5d83cd02998d5773230029
19130004 <19130004@st.hcmuaf.edu.vn>
2023-12-26 17:25:10 +0700

    create SecurityConfig in project

--- src/main/java/com/springboot/blogapp/blogapprestapi/config/SecurityConfig.java
+++ src/main/java/com/springboot/blogapp/blogapprestapi/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.springboot.blogapp.blogapprestapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,7 +17,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+    private UserDetailsService userDetailsService;
+    SecurityConfig(UserDetailsService userDetailsService){
+        this.userDetailsService = userDetailsService;
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+    @Bean
     public  static PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
@@ -21,17 +35,19 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth -> {
 //                    auth.anyRequest().authenticated();
-            auth.requestMatchers("/api/post/**").permitAll().anyRequest().authenticated();
+            auth.requestMatchers("/api/posts/**").permitAll().anyRequest().authenticated();
                 })
                 .httpBasic(Customizer.withDefaults());
         http.csrf(csrf -> csrf.disable());
+
         return http.build();
     }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder().username("admin").
-                password(passwordEncoder().encode("admin")).build();
 
-        return new InMemoryUserDetailsManager(user);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user = User.builder().username("admin").
+//                password(passwordEncoder().encode("admin")).build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
